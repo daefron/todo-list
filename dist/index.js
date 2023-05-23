cdce2076433399e1367c8f8edef744b40a267c57
Daefron <thomas_evans@outlook.com>
2023-05-23 14:24:17 +1000

    created project/todo creation logic

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,101 @@
+const projectMaker = (() => {
+  let projectHolder = [];
+  function Project(position, title, todoHolder) {
+    this.position = position;
+    this.title = title;
+    this.todoHolder = todoHolder;
+  }
+  function Todo(project, position, title, description, dueDate, priority, checked) {
+    this.project = project;
+    this.position = position;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checked = checked;
+  }
+  projectHolder.push(new Project(0, "test project 1", []));
+  projectHolder
+    .at(0)
+    .todoHolder.push(
+      new Todo(
+        0,
+        0,
+        "test todo 1",
+        "this is the first test todo",
+        "29/03/2024",
+        "high",
+        true,
+      )
+    );
+  console.log(projectHolder.at(0).todoHolder);
+  console.log(projectHolder.at(0));
+  const projects = document.getElementById("projects");
+  const todos = document.getElementById("mainright");
+
+  function projectToDisplay(Project) {
+    let projectDiv = document.createElement("div");
+    projectDiv.classList.add("project");
+    projects.appendChild(projectDiv);
+
+    let projectTitle = document.createElement("p");
+    projectTitle.textContent = Project.title;
+    projectDiv.appendChild(projectTitle);
+
+    let projectEdit = document.createElement("img");
+    projectEdit.classList.add("edit");
+    projectEdit.src = "text-box-edit.svg";
+    projectDiv.appendChild(projectEdit);
+
+    let projectDelete = document.createElement("img");
+    projectDelete.classList.add("delete");
+    projectDelete.src = "delete-forever.svg";
+    projectDelete.setAttribute("onclick", "buttons.projectDel()");
+    projectDiv.appendChild(projectDelete);
+  }
+  projectToDisplay(projectHolder[0]);
+  
+  function todoToDisplay (todo) {
+    let todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    todos.appendChild(todoDiv);
+
+    let todoChecked = document.createElement("input");
+    todoChecked.setAttribute("type", "checkbox");
+    todoDiv.appendChild(todoChecked);
+
+    let todoTitle = document.createElement("p");
+    todoTitle.textContent = todo.title;
+    todoDiv.appendChild(todoTitle);
+
+    let todoDueDate = document.createElement("p");
+    todoDueDate.textContent = todo.dueDate;
+    todoDiv.appendChild(todoDueDate);
+
+    let todoPriority = document.createElement("p");
+    todoPriority.textContent = todo.priority;
+    todoDiv.appendChild(todoPriority);
+
+    let editDiv = document.createElement("div");
+    todoDiv.appendChild(editDiv);
+
+    let todoEdit = document.createElement("img");
+    todoEdit.classList.add("edit");
+    todoEdit.src = "text-box-edit.svg";
+    editDiv.appendChild(todoEdit);
+
+    let delDiv = document.createElement("div");
+    todoDiv.appendChild(delDiv);
+
+    let todoDel = document.createElement("img");
+    todoDel.classList.add("delete");
+    todoDel.src = "delete-forever.svg";
+    todoDel.setAttribute("onclick", "buttons.todoDel()");
+    delDiv.appendChild(todoDel);
+  }
+  todoToDisplay(projectHolder.at(0).todoHolder[0]);
+})();
+
 const buttons = (() => {
   const blackout = document.getElementById("blackout");
   const projectModal = document.getElementById("projectModal");
@@ -27,6 +125,12 @@ const buttons = (() => {
     todoDelModal.style.visibility = "visible";
     blackout.style.visibility = "visible";
   };
+  document.querySelector(".project").addEventListener("click", () => {
+    document.querySelector(".project").style["background-color"] =
+      "rgb(212, 212, 212)";
+    document.querySelector(".project > .edit").style.visibility = "visible";
+    document.querySelector(".project > .delete").style.visibility = "visible";
+  });
   return {
     createProject,
     createTodo,
